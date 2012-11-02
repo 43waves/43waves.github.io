5325148c57ad1b5a042893f7f5a99eb99f476a8c
kumabotz <kumabotz@gmail.com>
2012-11-02 14:56:55 +0800

    Refresh blog

---
+++ javascripts/application.js
@@ -0,0 +1,25 @@
+jQuery(document).ready(function($){
+  $('#twitter').mouseover(function(){
+    $('#mousing').text("Twitter");
+  });
+
+  $('#twitter').mouseout(function(){
+    $('#mousing').html("&nbsp;");
+  });
+
+  $('#play').mouseover(function(){
+    $('#mousing').text("Android Apps");
+  });
+
+  $('#play').mouseout(function(){
+    $('#mousing').html("&nbsp;");
+  });
+
+  $('#about').mouseover(function(){
+    $('#mousing').text("About");
+  });
+
+  $('#about').mouseout(function(){
+    $('#mousing').html("&nbsp;");
+  });
+});
