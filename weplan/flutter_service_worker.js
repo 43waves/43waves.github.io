0e12bad040e160ff89302e36bada4cfec4050635
Tery Lim <kumabotz@gmail.com>
2026-03-20 21:19:58 +1300

    deploy: weplan web from feature/sync-log-diagnostic-statuses

--- weplan/flutter_service_worker.js
+++ weplan/flutter_service_worker.js
@@ -30,7 +30,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "firebase-messaging-sw.js": "d51b236b7dbab7eca4c92981bce99b22",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "15db3ed0dcf87668ddf26e4afa434775",
+"flutter_bootstrap.js": "92063d9be9efca741618aaa747ddea0b",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
