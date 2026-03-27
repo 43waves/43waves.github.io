49b91a9105468675cb7864a77e7151f0b608bab8
Tery Lim <kumabotz@gmail.com>
2026-03-27 18:42:42 +1300

    deploy weplan and weplan2 web builds

--- weplan/flutter_bootstrap.js
+++ weplan/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"d2913632a4578ee4d0b8b1c4a69888c8a0672c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1495296040"
+    serviceWorkerVersion: "1249688627"
   }
 });
