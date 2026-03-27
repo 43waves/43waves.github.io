195c0a2f2a579ab8790cdb829105cd583b0506a3
Tery Lim <kumabotz@gmail.com>
2026-03-27 19:20:22 +1300

    fix: add gstatic.com and fonts.gstatic.com to CSP connect-src

--- weplan2/flutter_bootstrap.js
+++ weplan2/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"d2913632a4578ee4d0b8b1c4a69888c8a0672c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3532240664"
+    serviceWorkerVersion: "3858624720"
   }
 });
