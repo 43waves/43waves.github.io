6b5c77c368f782a16c36bc715890cc014d38b770
Tery Lim <kumabotz@gmail.com>
2026-03-27 19:34:24 +1300

    fix: share calendar shows Shared successfully instead of Invited

--- weplan/flutter_bootstrap.js
+++ weplan/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"d2913632a4578ee4d0b8b1c4a69888c8a0672c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3363337441"
+    serviceWorkerVersion: "4193941655"
   }
 });
